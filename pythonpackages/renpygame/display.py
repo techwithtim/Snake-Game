4b2ad2e8ae60248616e8e6c1d7a532535de55a43
Exodia <67595890+DonRP@users.noreply.github.com>
2023-03-26 18:05:54 +0200

    MainSurface

--- pythonpackages/renpygame/display.py
+++ pythonpackages/renpygame/display.py
@@ -27,11 +27,35 @@ class Surface():
         return
 
 
+class MainSurface():
+    """https://www.pygame.org/docs/ref/surface.html"""
+
+    def __init__(
+            self,
+            size=(0, 0),
+            flags=0,
+            depth=0,
+            masks=None
+    ):
+
+        self.size = size
+        self.dest = None
+
+    def blit(
+            self,
+            dest: str,  # img
+            area=None,
+            special_flags=0
+    ):
+        self.dest = dest
+        return
+
+
 def mode_ok(size, flags=0, depth=0, display=0) -> int:
     """https://www.pygame.org/docs/ref/display.html#pygame.display.mode_ok"""
     return 0
 
 
-def set_mode(size=(0, 0), flags=0, depth=0, display=0, vsync=0):  # -> Surface:
+def set_mode(size=(0, 0), flags=0, depth=0, display=0, vsync=0) -> MainSurface:
     """https://www.pygame.org/docs/ref/display.html#pygame.display.set_mode"""
-    return  # Surface(size, flags, depth)
+    return MainSurface()(size, flags, depth)
