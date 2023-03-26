2639b03d2b398e1aadff783173497d7268ba94dc
Exodia <67595890+DonRP@users.noreply.github.com>
2023-03-26 17:51:46 +0200

    __all__

--- pythonpackages/renpygame/display.py
+++ pythonpackages/renpygame/display.py
@@ -27,9 +27,9 @@ class Surface():
         return
 
 
-def mode_ok(size, flags=0, depth=0, display=0):  # -> depth:
+def mode_ok(size, flags=0, depth=0, display=0) -> int:
     """https://www.pygame.org/docs/ref/display.html#pygame.display.mode_ok"""
-    return
+    return 0
 
 
 def set_mode(size=(0, 0), flags=0, depth=0, display=0, vsync=0):  # -> Surface:
---
+++ pythonpackages/renpygame/locals.py
@@ -0,0 +1,7 @@
+import pythonpackages.renpygame.display as display
+from pythonpackages.renpygame.display import Surface
+
+__all__ = [
+    "Surface",
+    "display",
+]
