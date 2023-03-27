b534fe65471b0debf9abff60c856c01b710518db
Exodia <67595890+DonRP@users.noreply.github.com>
2023-03-27 16:17:56 +0200

    Rect

--- pythonpackages/renpygame/rect.py
+++ pythonpackages/renpygame/rect.py
@@ -5,6 +5,7 @@ import renpy.exports as renpy
 
 
 class Rect(renpy.Displayable):
+    """https://github.com/renpy/pygame_sdl2/blob/364d3bcfa005b0eaf897d38b336a97fa6e05e76b/src/pygame_sdl2/rect.pyx#L28"""
 
     def __init__(
         self,
@@ -22,11 +23,10 @@ class Rect(renpy.Displayable):
         # The child.
         self.image: Optional[renpy.Displayable] = None
 
-        self.left = left
-        self.top = top
-        self.width = width
-        self.height = height
-        self.size = (width, height)
+        self.x = left
+        self.y = top
+        self.w = width
+        self.h = height
 
     def render(self, width: int, height: int, st: DOUBLE, at: DOUBLE):
         # Create the render we will return.
@@ -44,3 +44,147 @@ class Rect(renpy.Displayable):
 
         # Return the render.
         return render
+
+    @property
+    def left(self):
+        return self.x
+
+    @left.setter
+    def left(self, value):
+        self.x = value
+
+    @property
+    def top(self):
+        return self.y
+
+    @top.setter
+    def top(self, value):
+        self.y = value
+
+    @property
+    def width(self):
+        return self.w
+
+    @width.setter
+    def width(self, value):
+        self.w = value
+
+    @property
+    def height(self):
+        return self.h
+
+    @height.setter
+    def height(self, value):
+        self.h = value
+
+    @property
+    def right(self):
+        return self.x + self.width
+
+    @right.setter
+    def right(self, value):
+        self.x += value - self.right
+
+    @property
+    def bottom(self):
+        return self.y + self.height
+
+    @bottom.setter
+    def bottom(self, value):
+        self.y += value - self.bottom
+
+    @property
+    def size(self):
+        return (self.w, self.h)
+
+    @size.setter
+    def size(self, value):
+        self.w, self.h = value
+
+    @property
+    def topleft(self):
+        return (self.left, self.top)
+
+    @topleft.setter
+    def topleft(self, value):
+        self.left, self.top = value
+
+    @property
+    def topright(self):
+        return (self.right, self.top)
+
+    @topright.setter
+    def topright(self, value):
+        self.right, self.top = value
+
+    @property
+    def bottomright(self):
+        return (self.right, self.bottom)
+
+    @bottomright.setter
+    def bottomright(self, value):
+        self.right, self.bottom = value
+
+    @property
+    def bottomleft(self):
+        return (self.left, self.bottom)
+
+    @bottomleft.setter
+    def bottomleft(self, value):
+        self.left, self.bottom = value
+
+    @property
+    def centerx(self):
+        return self.x + (self.w / 2)
+
+    @centerx.setter
+    def centerx(self, value):
+        self.x += value - self.centerx
+
+    @property
+    def centery(self):
+        return self.y + (self.h / 2)
+
+    @centery.setter
+    def centery(self, value):
+        self.y += value - self.centery
+
+    @property
+    def center(self):
+        return (self.centerx, self.centery)
+
+    @center.setter
+    def center(self, value):
+        self.centerx, self.centery = value
+
+    @property
+    def midtop(self):
+        return (self.centerx, self.top)
+
+    @midtop.setter
+    def midtop(self, value):
+        self.centerx, self.top = value
+
+    @property
+    def midleft(self):
+        return (self.left, self.centery)
+
+    @midleft.setter
+    def midleft(self, value):
+        self.left, self.centery = value
+
+    @property
+    def midbottom(self):
+        return (self.centerx, self.bottom)
+
+    @midbottom.setter
+    def midbottom(self, value):
+        self.centerx, self.bottom = value
+
+    @property
+    def midright(self):
+        return (self.right, self.centery)
+
+    @midright.setter
+    def midright(self, value):
+        self.right, self.centery = value
