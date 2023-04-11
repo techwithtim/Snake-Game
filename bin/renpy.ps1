b9be3d4fdd55a9bcfa18ccd4dc548e6b0367579a
Exodia <67595890+DonRP@users.noreply.github.com>
2023-04-11 15:10:25 +0200

    fix

--- bin/renpy.ps1
+++ bin/renpy.ps1
@@ -4,5 +4,26 @@ param(
 $RenPy = Get-Content (Resolve-Path ".renpy-sdk")
 $RenPy = [string]::join("", ($RenPy.Split("`n")))
 
-Write-Output "Using Ren'Py SDK: $RenPy"
-Start-Process -FilePath "$RenPy\renpy.exe" -ArgumentList ". $Command"
+
+if ($IsWindows) {
+    Write-Output "Renpy will run on a Windows system"
+    Write-Output "Using Ren'Py SDK: $RenPy\renpy.exe"
+    Start-Process -FilePath "$RenPy\renpy.exe" -ArgumentList ". $Command"
+    exit
+}
+if ($IsLinux) {
+    Write-Output "Renpy will run on a Linux system"
+    Write-Output "Using Ren'Py SDK: $RenPy/renpy.sh"
+    Start-Process -FilePath "$RenPy/renpy.sh" -ArgumentList ". $Command"
+    exit
+}
+if ($IsMacOS) {
+    Write-Output "Renpy will run on a MacOS system"
+    Write-Output "Using Ren'Py SDK: $RenPy/renpy.sh"
+    Start-Process -FilePath "$RenPy/renpy.sh" -ArgumentList ". $Command"
+    exit
+}
+
+Write-Output "Renpy will run on an unknown system, may not work"
+Write-Output "Using Ren'Py SDK: $RenPy/renpy.sh"
+Start-Process -FilePath "$RenPy/renpy.sh" -ArgumentList ". $Command"
