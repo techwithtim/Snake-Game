6f1447a317b7e7ec4c02c9a3f955d60188e08f78
Exodia <67595890+DonRP@users.noreply.github.com>
2023-04-11 16:02:32 +0200

    improve

--- bin/renpy.ps1
+++ bin/renpy.ps1
@@ -8,22 +8,22 @@ if ($IsWindows -or $IsLinux -or $IsMacOS) {
     if ($IsWindows) {
         Write-Output "Renpy will run on a Windows system"
         Write-Output "Using Ren'Py SDK: $RenPy\renpy.exe"
-        Start-Process -FilePath "$RenPy\renpy.exe" -ArgumentList ". $Command"
+        Start-Process -FilePath "$RenPy\renpy.exe" -ArgumentList ". $Command" -Wait
     }
     if ($IsLinux) {
         Write-Output "Renpy will run on a Linux system"
         Write-Output "Using Ren'Py SDK: $RenPy/renpy.sh"
-        Start-Process -FilePath "$RenPy/renpy.sh" -ArgumentList ". $Command"
+        Start-Process -FilePath "$RenPy/renpy.sh" -ArgumentList ". $Command" -Wait
     }
     if ($IsMacOS) {
         Write-Output "Renpy will run on a MacOS system"
         Write-Output "Using Ren'Py SDK: $RenPy/renpy.sh"
-        Start-Process -FilePath "$RenPy/renpy.sh" -ArgumentList ". $Command"
+        Start-Process -FilePath "$RenPy/renpy.sh" -ArgumentList ". $Command" -Wait
     }
 } 
 else {
     Write-Output "Renpy will run on an unknown system, may not work"
     Write-Output "Using Ren'Py SDK: $RenPy/renpy.sh"
-    Start-Process -FilePath "$RenPy/renpy.sh" -ArgumentList ". $Command"
+    Start-Process -FilePath "$RenPy/renpy.sh" -ArgumentList ". $Command" -Wait
 }
 
