ea905a4e5f3d68f04246a6819e4b7656406ad8d2
Exodia <67595890+DonRP@users.noreply.github.com>
2023-04-18 15:38:58 +0200

    RenpyGameImage

--- pythonpackages/renpygame/image.py
+++ pythonpackages/renpygame/image.py
@@ -5,41 +5,55 @@ from pygame_sdl2.image import *
 import pythonpackages.renpygame.pygame as pygame
 
 
-class RenpyGameImage(renpy.Render):
-    """pygame: https://www.pygame.org/docs/ref/surface.html
-    pygame_sdl2: https://github.com/renpy/pygame_sdl2/blob/48e9c45667152a4ccf98d6d9251eeb3c8858b5f1/src/pygame_sdl2/surface.pyx#L53"""
+class RenpyGameImage():
+    """renpy.Image: https://github.com/renpy/renpy/blob/fb803ea05cca1b933f18d51fb0398d9545879af9/renpy/display/core.py#L292
+    renpy.Image: https://github.com/renpy/renpy/blob/fb803ea05cca1b933f18d51fb0398d9545879af9/renpy/ast.py#L1198"""
 
     def __init__(
         self,
-        displayable: renpy.Displayable,
+        path: str,
     ):
-        self.displayable = displayable
+        self.path = path
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @path.setter
+    def path(self, value: str) -> None:
+        self._path = value
 
     @property
     def displayable(self) -> renpy.Displayable:
-        return self._displayable
+        return renpy.displayable(self.path)
 
-    @displayable.setter
-    def displayable(self, value: renpy.Displayable) -> None:
-        self._displayable = value
-        self._width, self._height = self.displayable.get_size()
+    @property
+    def size(self) -> tuple[int, int]:
+        return self.pygame_image.get_size()
 
     @property
     def width(self) -> int:
-        return self._width
+        width, _ = self.size
+        return width
 
     @property
     def height(self) -> int:
-        return self._height
+        _, height = self.size
+        return height
 
-    def convert(self):
+    @property
+    def file(self):
+        return renpy.open_file(self.path)
+
+    @property
+    def pygame_image(self) -> pygame.Surface:
+        return pygame.image.load(self.file)
+
+    def convert(self) -> renpy.Render:
         return renpy.render(self.displayable, self.width, self.height)
 
 
 def load(file: str) -> Optional[RenpyGameImage]:
     """https://www.pygame.org/docs/ref/image.html#pygame.image.load"""
-    if isinstance(file, str):
-        displayable = renpy.displayable(file)
-        return RenpyGameImage(displayable)
-    else:
-        return None
+    displayable = renpy.displayable(file)
+    return RenpyGameImage(displayable)
