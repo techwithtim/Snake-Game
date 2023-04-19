8c98d307acafb2a6093d5c038c4aefa6d64e7d03
Exodia <67595890+DonRP@users.noreply.github.com>
2023-04-19 18:23:52 +0200

    fix

--- game/aliens.py
+++ game/aliens.py
@@ -17,21 +17,6 @@ SCREENRECT = Rect(0, 0, 640, 480)
 SCORE = 0
 
 
-def load_image(file):
-    "loads an image, prepares it for play"
-    file = os_path_join('images', file)
-    file = renpy.open_file(file)
-    surface = pygame.image.load(file)
-    return surface.convert()
-
-
-def load_images(*files):
-    imgs = []
-    for file in files:
-        imgs.append(load_image(file))
-    return imgs
-
-
 class dummysound:
     def play(self): pass
 
