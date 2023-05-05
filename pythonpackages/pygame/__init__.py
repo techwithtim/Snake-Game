c28412b42047501ce147e5e0f2b5c8384c30344b
Exodia <67595890+DonRP@users.noreply.github.com>
2023-05-05 10:11:51 +0200

    improve

--- pythonpackages/pygame/__init__.py
+++ pythonpackages/pygame/__init__.py
@@ -1,1 +1,15 @@
 from pygame_sdl2 import *
+
+import pygame.display as my_display
+import pygame.event as my_event
+import pygame.image as my_image
+import pygame.rect as my_rect
+import pygame.sprite as my_sprite
+import pygame.transform as my_transform
+
+rect = my_rect
+display = my_display
+sprite = my_sprite
+image = my_image
+transform = my_transform
+event = my_event
