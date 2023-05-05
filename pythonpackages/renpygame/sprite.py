54f39a1bb4c1f405906996e0d24aa431bb94ef78
Exodia <67595890+DonRP@users.noreply.github.com>
2023-05-05 12:36:08 +0200

    improve

--- pythonpackages/renpygame/sprite.py
+++ pythonpackages/renpygame/sprite.py
@@ -11,25 +11,25 @@ class Sprite(pygame.sprite.Sprite):
 
     def __init__(self, *groups, **kwargs):
         # Sprite init
-        pygame.sprite.Sprite.__init__(self, *groups, **kwargs)
+        super().__init__(*groups, **kwargs)
 
     def add(self, *groups):
-        super().add(*groups)
+        return super().add(*groups)
 
     def remove(self, *groups):
-        super().remove(*groups)
+        return super().remove(*groups)
 
     def add_internal(self, group):
-        super().add_internal(group)
+        return super().add_internal(group)
 
     def remove_internal(self, group):
-        super().remove_internal(group)
+        return super().remove_internal(group)
 
     def update(self, *args):
-        super().update(*args)
+        return super().update(*args)
 
     def kill(self):
-        super().kill()
+        return super().kill()
 
     def groups(self):
         return super().groups()
@@ -44,19 +44,19 @@ class AbstractGroup(pygame.sprite.AbstractGroup):
 
     def AbstractGroup(self):
         # Sprite init
-        pygame.sprite.AbstractGroup.__init__(self)
+        super().__init__()
 
     def sprites(self) -> list[Sprite]:
         return super().sprites()
 
     def add_internal(self, sprite):
-        super().add_internal(sprite)
+        return super().add_internal(sprite)
 
     def remove_internal(self, sprite):
-        super().remove_internal(sprite)
+        return super().remove_internal(sprite)
 
     def has_internal(self, sprite):
-        super().has_internal(sprite)
+        return super().has_internal(sprite)
 
     def copy(self):
         return super().copy()
@@ -83,37 +83,19 @@ class AbstractGroup(pygame.sprite.AbstractGroup):
         return super().empty()
 
 
-class Group(AbstractGroup):
+class Group(pygame.sprite.Group):
     """pygame:
     pygame_sdl2: https://github.com/renpy/pygame_sdl2/blob/master/src/pygame_sdl2/sprite.py#L531
     """
 
     def __init__(self, *sprites):
-        AbstractGroup.__init__(self)
-        self.add(*sprites)
+        super().__init__(*sprites)
 
 
-class RenderUpdates(Group):
+class RenderUpdates(pygame.sprite.RenderUpdates):
     """pygame: https://www.pygame.org/docs/ref/sprite.html#pygame.sprite.RenderUpdates
     pygame_sdl2: https://github.com/renpy/pygame_sdl2/blob/master/src/pygame_sdl2/sprite.py#L557
     """
 
     def draw(self, surface: Surface) -> list[Rect]:
-        spritedict = self.spritedict
-        surface_blit = surface.blit
-        dirty = self.lostsprites
-        self.lostsprites = []
-        dirty_append = dirty.append
-        for s in self.sprites():
-            r = spritedict[s]
-            newrect = surface_blit(s.image, s.rect)
-            if r:
-                if newrect.colliderect(r):
-                    dirty_append(newrect.union(r))
-                else:
-                    dirty_append(newrect)
-                    dirty_append(r)
-            else:
-                dirty_append(newrect)
-            spritedict[s] = newrect
-        return dirty
+        return super().draw(surface)
